ee2c9a94a25949093ed7c7cd7288728c6db13499
Shafiujjaman-shafi <jcmshafi96@gmail.com>
2022-12-08 01:09:14 +0600

    working with splashScreen

---
+++ app/src/main/java/com/cse464/wechat/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.cse464.wechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        },SPLASH_TIMER);
+
+
+
+
+
+    }
+}
