804f53b71e98cc1a25e66e9701553f45717f4b9b
Shafiujjaman-shafi <jcmshafi96@gmail.com>
2022-12-08 02:58:30 +0600

    working with setProfile

---
+++ app/src/main/java/com/cse464/wechat/setProfile.java
@@ -0,0 +1,14 @@
+package com.cse464.wechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_profile);
+    }
+}
